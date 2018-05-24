1044a81eebc3fe3b262ede8a9fcd30fcf34aa8cf
rhys <rhys@http://code.smartstudy.com/>
2018-05-24 17:32:22 +0800

    clean the code

--- decode_about.py
+++ decode_about.py
@@ -10,16 +10,14 @@ def decode(preds,lens,lstm_decoder,alpha):
       pred=preds[:,batch_index,:]
       for xi_index in range(lens[batch_index]):
          xi=pred[xi_index][:]
-         flag=True
          loop_count=0
-         while flag:
+         while True:
            loop_count+=1
            y_i=y[0][0][:]
            ytu = nn.functional.softmax(y_i+xi,dim=0)
            max_value,yi=torch.max(ytu,0)
            yi_index=int(yi.data)
-           if loop_count>1 and yi_index==yi_index_old:
-              flag=False
+           if (loop_count>1 and yi_index==yi_index_old) or yi_index==0:
               break
            if yi_index!=0:
               yi_index_old=yi_index
@@ -29,6 +27,4 @@ def decode(preds,lens,lstm_decoder,alpha):
               t_onehot=t_onehot.permute(1,0,2) #btn
               y, hid1,hid2 = lstm_decoder.decode(Variable(t_onehot).cuda(),hid1,hid2)
               y_seq[batch_index]+=alpha[yi_index-1]
-           else:
-              flag=False
     return y_seq
